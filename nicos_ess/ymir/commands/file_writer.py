eb862cea08c01deb1d56ccceb9f1268464f24746
AÜC Hardal <umit.hardal@ess.eu>
2020-11-30 13:45:18 +0100

    Start stop writing

--- nicos_ess/ymir/commands/file_writer.py
+++ nicos_ess/ymir/commands/file_writer.py
@@ -1,51 +1,39 @@
-from nicos import session
-from nicos.commands import helparglist, usercommand
-from file_writer_control.WorkerCommandChannel import WorkerCommandChannel
-from file_writer_control.WriteJob import WriteJob
-from file_writer_control.JobHandler import JobHandler
-from datetime import datetime, timedelta
-
-import time
-import os
-import json
+from nicos.commands import usercommand
+from nicos_ess.ymir.commands.start_stop_writing import StartFileWriter,\
+    StopFileWriter
 
 
-def _prepare_write_job(host, topic, config, start_time):
-    command_channel = WorkerCommandChannel(f'{host}/{topic}')
-    job_handler = JobHandler(worker_finder=command_channel)
-    with open(config, "r") as f:
-        nexus_structure = f.read()
-    write_job = WriteJob(
-        nexus_structure,
-        "{0:%Y}-{0:%m}-{0:%d}_{0:%H}{0:%M}.nxs".format(start_time),
-        host,
-        start_time,
-    )
-    return job_handler, write_job
+class SetGetHandler:
+    """
+    An auxiliary class for setting and getting the file writer handler
+    object. This class is needed as the corresponding class of starting a write
+    job should instantiate only once.
+    """
+    def __init__(self):
+        self.handler = None
+
+    def set_handler(self, handler_value):
+        self.handler = handler_value
+
+    def get_handler(self):
+        return self.handler
+
+
+# Global initialisation is required to not to override the handler value
+# one initiated via a write call.
+sg_handler = SetGetHandler()
 
 
 @usercommand
 def start_writing():
-    _device = session.getDevice('FileWriterParameters')
-    kafka_host = _device.broker[0]
-    topic = _device.command_topic
-    config = _device.nexus_config_path
-
-    _time = datetime.now()
-    handler, job = _prepare_write_job(kafka_host, topic, config, _time)
-
-    print("Requesting writing start")
-    start_handler = handler.start_job(job)
-    while not start_handler.is_done():
-        time.sleep(1)
-    # stop_time = _time + timedelta(seconds=10)
-    # stop_handler = handler.set_stop_time(stop_time)
-    # while not stop_handler.is_done():
-    #     time.sleep(1)
-    # while not handler.is_done():
-    #     time.sleep(1)
-    # print("Write job is done")
+    writer = StartFileWriter()
+    writer.start_job()
+    # Set the handler which is to be used to stop the write job.
+    sg_handler.set_handler(writer.get_handler())
+    print('Write job is started.')
 
 
+@usercommand
 def stop_writing():
-    pass
+    StopFileWriter(sg_handler.get_handler()).stop_job()
+    print('Write job is stopped.')
---
+++ nicos_ess/ymir/commands/start_stop_writing.py
@@ -0,0 +1,52 @@
+from nicos import session
+from file_writer_control.WorkerCommandChannel import WorkerCommandChannel
+from file_writer_control.WriteJob import WriteJob
+from file_writer_control.JobHandler import JobHandler
+from datetime import datetime
+
+import time
+
+
+class StartFileWriter:
+    def __init__(self):
+
+        self.device = session.getDevice('FileWriterParameters')
+        self.host = self.device.broker[0]
+        self.config = self.device.nexus_config_path
+        self.topic = self.device.command_topic
+
+        self.command_channel = WorkerCommandChannel(f'{self.host}/{self.topic}')
+        self.job_handler = JobHandler(worker_finder=self.command_channel)
+        self.write_job = None
+        self.job_id = None
+
+    def start_job(self):
+        with open(self.config, "r") as f:
+            nexus_structure = f.read()
+        self.write_job = WriteJob(
+            nexus_structure,
+            "{0:%Y}-{0:%m}-{0:%d}_{0:%H}{0:%M}.nxs".format(datetime.now()),
+            self.host,
+            datetime.now(),
+        )
+        start_handler = self.job_handler.start_job(self.write_job)
+        self.job_id = self.write_job.job_id
+        print(self.job_id)
+        print(self.write_job)
+        while not start_handler.is_done():
+            time.sleep(1)
+
+    def get_handler(self):
+        return self.job_handler
+
+
+class StopFileWriter:
+    def __init__(self, handler):
+        self.job_handler = handler
+
+    def stop_job(self):
+        stop_handler = self.job_handler.stop_now()
+        while not stop_handler.is_done():
+            time.sleep(1)
+        while not self.job_handler.is_done():
+            time.sleep(1)
