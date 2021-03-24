b32b683849f1c68f97c8bbce6cdf554b432c9f72
Ebad Kamil <ebad.kamil@ess.eu>
2021-03-24 11:22:49 +0100

    Add validators in utilities folder

---
+++ nicos_ess/gui/utilities/delegates.py
@@ -0,0 +1,37 @@
+from nicos.guisupport.qt import QLineEdit, QStyledItemDelegate, QIntValidator, \
+    QDoubleValidator, Qt, QRegExpValidator, QRegExp
+
+
+class Validators:
+    class PIntValidator(QIntValidator):
+        def __init__(self, parent=None):
+            super().__init__(parent=parent)
+            self.setBottom(0)
+
+    class PDoubleValidator(QDoubleValidator):
+        def __init__(self, parent=None):
+            super().__init__(parent=parent)
+            self.setBottom(0.)
+
+
+    integer = QIntValidator()
+    pinteger = PIntValidator()
+    double = QDoubleValidator()
+    pdouble = PDoubleValidator()
+    string = QRegExpValidator(QRegExp('^(?!\s*$).+'))
+
+
+class Delegate(QStyledItemDelegate):
+    def __init__(self, owner, validator_type):
+        super().__init__(owner)
+        self.validator_type = validator_type
+
+    def createEditor(self, parent, option, index):
+        editor = QLineEdit(parent)
+        editor.setValidator(self.validator_type)
+        return editor
+
+    def setModelData(self, editor, model, index):
+        text = editor.text()
+        model.setData(index, text, Qt.EditRole)
+
