74141fee48232c2b1cfb3bfd7aa519c39f781227
Matt Clarke <matt.clarke@esss.se>
2021-04-12 21:05:55 +0200

    instrument state before updating

--- tmux_scripts/start_nicos_ymir.sh
+++ tmux_scripts/start_nicos_ymir.sh
@@ -9,21 +9,21 @@ tmux send-keys "export EPICS_CA_AUTO_ADDR_LIST=NO" C-m
 tmux send-keys "export EPICS_CA_ADDR_LIST='172.30.238.79'" C-m
 tmux send-keys "export EPICS_PVA_AUTO_ADDR_LIST=NO" C-m
 tmux send-keys "export EPICS_PVA_ADDR_LIST='172.30.238.79'" C-m
-tmux send-keys "bin/nicos-cache" C-m
+tmux send-keys "INSTRUMENT=nicos_ess.ymir bin/nicos-cache" C-m
 
 tmux new-window -t $session:2 -n poller
 tmux send-keys "export EPICS_CA_AUTO_ADDR_LIST=NO" C-m
 tmux send-keys "export EPICS_CA_ADDR_LIST='172.30.238.79'" C-m
 tmux send-keys "export EPICS_PVA_AUTO_ADDR_LIST=NO" C-m
 tmux send-keys "export EPICS_PVA_ADDR_LIST='172.30.238.79'" C-m
-tmux send-keys "bin/nicos-poller" C-m
+tmux send-keys "INSTRUMENT=nicos_ess.ymir bin/nicos-poller" C-m
 
 tmux new-window -t $session:3 -n daemon
 tmux send-keys "export EPICS_CA_AUTO_ADDR_LIST=NO" C-m
 tmux send-keys "export EPICS_CA_ADDR_LIST='172.30.238.79'" C-m
 tmux send-keys "export EPICS_PVA_AUTO_ADDR_LIST=NO" C-m
 tmux send-keys "export EPICS_PVA_ADDR_LIST='172.30.238.79'" C-m
-tmux send-keys "bin/nicos-daemon" C-m
+tmux send-keys "INSTRUMENT=nicos_ess.ymir bin/nicos-daemon" C-m
 
 tmux new-window -t $session:4 -n forwarder
 tmux send-keys "export EPICS_CA_AUTO_ADDR_LIST=NO" C-m
